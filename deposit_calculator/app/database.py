69d90e1476af8247a8142031ca9f872705a5cc41
anakin <lord.al@icloud.com>
2025-01-13 01:55:03 +0400

    update .env + database

--- deposit_calculator/app/database.py
+++ deposit_calculator/app/database.py
@@ -1,15 +1,19 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
+
 from app.utils import get_database_url
 
-engine = create_async_engine(get_database_url(), pool_pre_ping=True)
-SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(get_database_url(), pool_pre_ping=True, echo=True)
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
 Base = declarative_base()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
+async def get_db():
+    async with SessionLocal() as db:
         yield db
-    finally:
-        db.close()
