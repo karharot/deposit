587b07dafe48e7fad540f9ec4b7fd74bbe4fe634
anakin <lord.al@icloud.com>
2025-01-28 16:49:35 +0400

    update alembic.ini + database

--- deposit_calculator/app/database.py
+++ deposit_calculator/app/database.py
@@ -1,9 +1,33 @@
+import os
+
+import yaml
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-from app.utils import get_database_url
 
-engine = create_async_engine(get_database_url(), pool_pre_ping=True, echo=True)
+CONFIG_FILE = "config.yaml"
+
+
+def get_database_url():
+    database_url = os.environ.get("DATABASE_URL")
+    if database_url:
+        return database_url
+
+    try:
+        with open(CONFIG_FILE, "r") as f:
+            config = yaml.safe_load(f)
+            database_url = config.get("DATABASE_URL")
+            if database_url:
+                return database_url
+    except FileNotFoundError:
+        print(f"Warning: Configuration file {CONFIG_FILE} not found.")
+    except Exception as e:
+        print(f"Error reading configuration file: {e}")
+
+
+DATABASE_URL = get_database_url()
+
+engine = create_async_engine(DATABASE_URL, pool_pre_ping=True, echo=True)
 SessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
